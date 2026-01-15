f570c51d6b62c7c31a41309555ea8c790ec8d7e6
Jamal Abudiab <jabudiab2024@gmail.com>
2026-01-15 19:55:42 +0000

    Runways.csv added as a dataset, requirements.txt is updated, submission.py created as the final product to answer questions(However, questions are incomplete).

---
+++ submission.py
@@ -0,0 +1,25 @@
+#What is your terminal display "path"? (type/paste as text into the .py file)
+# @J-CYN ➜ /workspaces/daytwostuff (main) $
+
+#Should you include the environment in your repo or not?
+
+#Now what is your terminal display "path"? Is it different?
+#(.venv) (base) @J-CYN ➜ /workspaces/daytwostuff (main) $
+
+#What do you notice about the extension menu?
+
+
+#What are three useful elements of Data Wrangler?
+#It shows the number of missing info in each column, gives simple count graphs for certain columns, and shows distinct variables for others.
+
+#Why do we use a requirements.txt file?
+#It allows others to be able to use the same libraries and versions of those libraries as us when they make their own virtual environment. It also allows for easy and quick downloads.
+
+import numpy as np
+import pandas as pd
+
+df=pd.read_csv("Runways.csv")
+
+print(df)
+
+
