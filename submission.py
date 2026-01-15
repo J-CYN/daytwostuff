41fcd0c582a9f80d3b0c61c696fe7bf23d0bc73f
Jamal Abudiab <jabudiab2024@gmail.com>
2026-01-15 20:14:15 +0000

    Question are basically answered

--- submission.py
+++ submission.py
@@ -2,12 +2,13 @@
 # @J-CYN ➜ /workspaces/daytwostuff (main) $
 
 #Should you include the environment in your repo or not?
+# No, as the environment will differ depending on the person's system, and can easily be recreated. It is better to not include it in the repo to allow others to create their own which is created with their device and OS in mind.
 
 #Now what is your terminal display "path"? Is it different?
-#(.venv) (base) @J-CYN ➜ /workspaces/daytwostuff (main) $
+# (.venv) (base) @J-CYN ➜ /workspaces/daytwostuff (main) $
 
 #What do you notice about the extension menu?
-
+# There is a local installed section and a codespaces install section that are separate
 
 #What are three useful elements of Data Wrangler?
 #It shows the number of missing info in each column, gives simple count graphs for certain columns, and shows distinct variables for others.
